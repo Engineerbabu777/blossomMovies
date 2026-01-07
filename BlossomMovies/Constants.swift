4c9e7bef64d535af963206ae49ef9dbd2dd585b1
EngineerBabu777 <awaismumtaz0099@gmail.com>
2026-01-07 19:18:56 +0500

    add(Constants): updated a bit

--- BlossomMovies/Constants.swift
+++ BlossomMovies/Constants.swift
@@ -14,4 +14,13 @@ struct Constants {
     static let searchString = "Search"
     static let downloadString = "Download"
     
+    static let homeIconString = "house"
+    static let upcomingIconString = "play.circle"
+    static let searchIconString = "magnifyingglass"
+    static let downloadIconString = "arrow.down.to.line"
+    
+    static let testTitleURL = "https://image.tmdb.org/t/p/w500/nnl6OWkyPpuMm595hmAxNW3rZFn.jpg"
+    static let testTitleURL2 = "https://image.tmdb.org/t/p/w500/d5iIlFn5s0ImszYzBPb8JPIfbXD.jpg"
+    static let testTitleURL3 = "https://image.tmdb.org/t/p/w500/qJ2tW6WMUDux911r6m7haRef0WH.jpg"
+       
 }
---
+++ BlossomMovies/HomeView.swift
@@ -0,0 +1,20 @@
+//
+//  HomeView.swift
+//  BlossomMovies
+//
+//  Created by Engineer Awais on 07/01/2026.
+//
+
+import SwiftUI
+
+struct HomeView: View {
+    
+    var heroTestTitle = ""
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    HomeView()
+}
