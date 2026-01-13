11a78316daa63588a4f94836ade8684a1ca21aea
EngineerBabu777 <awaismumtaz0099@gmail.com>
2026-01-14 00:26:04 +0500

    add(GeometryReader): this has been added

--- BlossomMovies/HomeView.swift
+++ BlossomMovies/HomeView.swift
@@ -10,38 +10,42 @@ import SwiftUI
 struct HomeView: View {
     
     var heroTestTitle = Constants.testTitleURL
+    
     var body: some View {
-        ScrollView {
-            LazyVStack{
-                AsyncImage(url: URL(string: heroTestTitle)){ image in
-                    image
-                        .resizable()
-                        .scaledToFit()
-                }placeholder:{
-                    ProgressView()
-                }
-                
-                HStack{
-                    Button{
-                        
-                    } label:{
-                        Text(Constants.playString)
-                            .ghostButton()
+        GeometryReader { geo in
+            ScrollView {
+                LazyVStack{
+                    AsyncImage(url: URL(string: heroTestTitle)){ image in
+                        image
+                            .resizable()
+                            .scaledToFit()
+                    }placeholder:{
+                        ProgressView()
                     }
+                    .frame(width: geo.size.width, height: geo.size.height*0.85)
                     
-                    Button{
+                    HStack{
+                        Button{
+                            
+                        } label:{
+                            Text(Constants.playString)
+                                .ghostButton()
+                        }
                         
-                    } label:{
-                        Text(Constants.downloadString)
-                            .ghostButton()
-                        
+                        Button{
+                            
+                        } label:{
+                            Text(Constants.downloadString)
+                                .ghostButton()
+                            
+                        }
                     }
                 }
+                HorizontalListView(header: Constants.trendingMoviesString)
+                HorizontalListView(header: Constants.trendingTVString)
+                HorizontalListView(header: Constants.topRatedMovieString)
+                HorizontalListView(header: Constants.topRatedTVString)
             }
-            HorizontalListView(header: Constants.trendingMoviesString)
-            HorizontalListView(header: Constants.trendingTVString)
-            HorizontalListView(header: Constants.topRatedMovieString)
-            HorizontalListView(header: Constants.topRatedTVString)
         }
        
         
